ff916d862b4ad5d73edb809564496b29ad2f66be
rv-us <rverma8@ucsc.edu>
2024-02-16 12:47:58 -0800

    tailwind integrated with current html file :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+console.log(process.env.NODE_ENV);
+const purge = process.env.NODE_ENV === 'production' ? true : false;
 module.exports = {
-  purge: [],
+  content: ['./build/**/*.html'],
   darkMode: true,
   theme: {
     extend: {},
